4b253479d14a58f3b6fea9a7bff192dc30bf91b9
calebcapps <caleb.capps@gentex.com>
2022-05-27 13:35:22 -0400

    added speech recongnition module

--- Python/Python Audio/Play audio with Python/Play_audio_with_Python.py
+++ Python/Python Audio/Play audio with Python/Play_audio_with_Python.py
@@ -1,5 +1,24 @@
-import matplotlib.pyplot
+import pyaudio
+import wave
 
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio()
 
+    stream = pa.open(
+            format = pa.get_format_from_width(wf.getsampwidth()),
+            channels = wf.getnchannels(),
+            rate = wf.getframerate(),
+            output = True
+        )
+    datastream = wf.readframes(chunk)
 
-print("Hello")
+    while datastream:
+        stream.write(datastream)
+        datastream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate
+
+play_audio("endsound.wav")
